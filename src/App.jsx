ee95e2dbcbd107438adc389368fa990423e08f98
saheel36239 <143160522+saheel36239@users.noreply.github.com>
2026-06-12 15:36:53 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import StudentDashboard from "./components/StudentDashboard";
-import AdminDashboard from "./components/AdminDashboard";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import AddComplaint from "./components/AddComplaint";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-       <Route path="/" element={<StudentDashboard />} />
 
-        <Route path="/dashboard" element={<StudentDashboard />} />
-
-        <Route path="/admindashboard" element={<AdminDashboard />} />
-        
+       < Route path="/" element={<AddComplaint  />} />
+<Route path="/addcomplaint" element={<AddComplaint />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
-  
