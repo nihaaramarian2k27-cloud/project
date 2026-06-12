05c000a5242562b8162cbfb7ed8074e07eeb7873
mukilal0 <mukilal005@gmail.com>
2026-06-12 15:47:06 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,44 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import MyComplaints from "./components/MyComplaints";
-import EditComplaint from "./components/EditComplaint";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
 
-       <Route path="/"element={<MyComplaints />} />
-        <Route path="/mycomplaints" element={<MyComplaints />} />
-        <Route path="/editcomplaint" element={<EditComplaint />} />
+        <Route
+          path="/"
+          element={
+            <div>
+              <h1>Campus Complaint Management System</h1>
+
+              <p>Report and Track Campus Complaints Easily</p>
+
+              <Link to="/login">
+                <button>Login</button>
+              </Link>
+
+              <Link to="/register">
+                <button>Register</button>
+              </Link>
+            </div>
+          }
+        />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+       
+        
+       
 
       </Routes>
     </BrowserRouter>
   );
 }
+
 export default App;
+
